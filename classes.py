5280ed229eb5724f5adaccfcceb5d7b47969e4b7
landjbs <landonsmith@college.harvard.edu>
2019-03-25 18:46:35 -0400

    counter + wordList init

--- classes.py
+++ classes.py
@@ -2,12 +2,21 @@ class Leaf:
     """
     Word following n previous words
     """
-    def __init__(self, name):
+    def __init__(self, name, counter):
         if type(name) is str:
             self.name = name
         else:
             raise ValueError("Usage: name must be type string")
 
+        if type(counter) is int:
+            self.counter = counter
+        else:
+            raise ValueError("Usage: counter must be type int")
+
+        if type(wordList) is list:
+            self.wordLsit = wordList
+        else:
+            raise ValueError("Usage: wordList must be type list")
 
 class Node:
     def __init__(self, ):
