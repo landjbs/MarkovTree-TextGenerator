8dc1aaf5fb60050450d3a108e140e062b2a67fe7
landjbs <landonsmith@college.harvard.edu>
2019-03-26 19:24:53 -0400

    tiny changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import time
 from importer import process_text_folder
 
-# timing stuff
+# time import of sample_text folder
 start = time.time()
 
 sample = process_text_folder("sample_texts")
 
 end = time.time()
 
-print(len(sample))
 print(end - start)
+print(len(sample))
