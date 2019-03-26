e2a240237211442b06af5bafeec50b03592c5abb
landjbs <landonsmith@college.harvard.edu>
2019-03-26 11:47:47 -0400

    txt checker read_file

--- importer.py
+++ importer.py
@@ -1,8 +1,15 @@
+import os, time
+from fnmatch import fnmatch
+
 def read_file(file):
     """
     Args: txt file to read
     Returns: string of contents of txt file
     """
+    # check if file is .txt
+    if not fnmatch(file, '*.txt'):
+        raise ValueError(f"Usage: {file} must be of type .txt")
+    # write txt file into string, line by line
     with open(file, encoding = 'utf8') as FileObj:
         text = "".join([line for line in FileObj])
     return text
@@ -35,8 +42,8 @@ def process_text(file):
     words = text.replace("\n", " ").split(" ")
     # clean all words in words list
     cleanedWords = list(map(clean_word, words))
+    # remove all empty strings (products of cleaning pure junk)
     processedWords = remove_empty(cleanedWords)
-
     return processedWords
 
 def process_text_folder(folder):
@@ -44,7 +51,13 @@ def process_text_folder(folder):
     Args: name of folder containing txt files
     Returns: list of all processed words in all texts
     """
-    processedWords = []
-    for file in os.listdir(folder):
-        processedWords += process_text(f"{folder}/{file}")
+    processedWords = [([] + process_text(f"{folder}/{file}") for file in os.listdir(folder))]
     return processedWords
+
+start = time.time()
+
+# sample = process_text('sample_texts/test.pdf')
+sample = process_text_folder("sample_texts")
+
+end = time.time()
+print(end - start)
