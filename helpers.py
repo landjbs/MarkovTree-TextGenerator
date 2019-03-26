2175606c1ff9d71ed8cd102750eb43ab1bb77ae7
Harrison-Oatman <hroatman@college.harvard.edu>
2019-03-25 23:50:06 -0400

    comments

--- helpers.py
+++ helpers.py
@@ -1,17 +1,29 @@
+'''
+purpose: determine which of two strings comes first alphabetically
+args: a - subject string
+      b - string to be checked against
+returns: 0 if words are the same
+         1 if subject string is alphabetically first
+         -1 if check string is alphabetically first
+         str_check("fo", "foo") returns 1
+'''
 def str_check(a,b):
+    #take strings to uppercase
     subject = a.upper()
     check = b.upper()
+    #determine if strings are the same
     if subject == check:
         return 0
+    #take length of shortest string
     length = min([len(subject),len(check)])
+    #iterate through letters to determine first string
     for i in range(length):
         if ord(subject[i]) > ord(check[i]):
             return -1
         elif ord(subject[i]) < ord(check[i]):
             return 1
+    #if the letters are the same, shortest string is first alphabetically
     if len(subject) > length:
         return -1
     else:
         return 1
-
-print(str_check("Landz", "Landone"))
