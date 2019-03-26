7fd82546309a29dcfee40a9905995d79b55eb8b8
landjbs <landonsmith@college.harvard.edu>
2019-03-26 19:28:35 -0400

    timing removed

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import time
 from importer import process_text_folder
 
-# time import of sample_text folder
-start = time.time()
-
 sample = process_text_folder("sample_texts")
-
-end = time.time()
-
-print(end - start)
 print(len(sample))
