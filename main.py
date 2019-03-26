145c2bec4bcd27fc08910d610763bd811617c770
landjbs <landonsmith@college.harvard.edu>
2019-03-26 12:18:45 -0400

    processer import into main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import numpy as np
-import string
-import os
+import time
 from importer import process_text_folder
+
+# timing stuff
+start = time.time()
 
 sample = process_text_folder("sample_texts")
 
+end = time.time()
+
 print(len(sample))
+print(end - start)
