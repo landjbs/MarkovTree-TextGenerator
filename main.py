a61d1a8bf6123bf955afbb4804c88519f41b8294
landjbs <landonsmith@college.harvard.edu>
2019-04-04 11:58:34 -0400

    updates to main

--- main.py
+++ main.py
@@ -6,24 +6,27 @@ SAMPLE_NAME = "sample_texts/prideandprejudice.txt"
 sample, type = process_mutable(SAMPLE_NAME)
 
 sample_length = len(sample)
-print(f"Loading {sample_length} samples from {SAMPLE_NAME} (type: {type})")
+print(f"Loading {sample_length} samples from {type}, {SAMPLE_NAME}")
 n = 10
 tree = Node(sample[0:n], sample[n])
 
 for i in range(1,sample_length-(n+1)):
-    print(f"Completion: {i/sample_length}%", end="\r")
+    print(f"|Completion: {round(i/sample_length, 2)}%", end="\r")
     tree.search_add(sample[i:i+n], sample[i+n+1])
+print("Import complete!")
 
 # print(sample[500:500+n], tree.nav(sample[500:500+n]))
 gen_words = 100
 gen_text = []
 
-user_seed = input("Seed:\n")
+# user_seed = input("Seed:\n")
+#
+# if (set(process_text(user_seed))).issubset(set(sample)):
+#     seed = process_text(user_seed)
+# else:
+#     raise ValueError("Usage: seed must be in sample")
 
-if (set(process_text(user_seed))).issubset(set(sample)):
-    seed = process_text(user_seed)
-else:
-    raise ValueError("Usage: seed must be in sample")
+seed = process_text("hello mother")
 
 for i in range(gen_words):
     next = False
@@ -33,4 +36,6 @@ for i in range(gen_words):
         order += -1
     seed.append(next)
 
-print(" ".join(seed))
+textString = " ".join(seed)
+
+print(f"GENERATED TEXT:\n{textString}\n")
